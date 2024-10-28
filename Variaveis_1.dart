d9e6a8776b656f2e67e368ebc5f7239ec1fc59d7
Arthur Rodrigues Braga <arthurrodriguesestudante@gmail.com>
2024-10-28 19:28:05 -0400

    Adicionando os arquivos
    Adicionando os arquivos da Aula de Variáveis

---
+++ Variaveis_1.dart
@@ -0,0 +1,7 @@
+main() {
+  //Para declarar a variável precisa = Declarar o tipo da variável | O nome dela = o valor dela
+  int numInt = 4;
+  double numDouble = 3.14;
+  print(numInt);
+  print(numDouble);
+}
---
+++ Variaveis_2.dart
@@ -0,0 +1,8 @@
+main() {
+  var num1 = 10;
+  var num2 = 42.324;
+  var texto = "O valor da soma é: ";
+  print(texto + (num1 + num2).toString());
+  //Para convertermos os valores para string, precisamos utilizar o comando ".toString"
+ 
+  }
